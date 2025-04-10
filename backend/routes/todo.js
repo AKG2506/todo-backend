616a4deaf8c9824bf368f53fff459d8e90fb6e8d
Akshat Gupta <93175613+AKG2506@users.noreply.github.com>
2025-04-10 14:00:20 +0530

    Update todo.js

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const router=express.Router();
-const auth=require("../middleware/authMiddleware");
+const auth=require("../Middleware/authMiddleware");
 const {getTodos,createTodo,toggleTodo,deleteTodo}=require("../controllers/todoController")
 router.get("/",auth,getTodos);
 router.post("/",auth,createTodo);
